15eb26c42440563bbed4568b2125ddffdd650b18
TicketIT <alexandra_florentina22@yahoo.com>
2021-04-20 22:10:28 +0300

    LoginActivity

--- app/src/main/java/com/example/ticketit/LoginActivity.kt
+++ app/src/main/java/com/example/ticketit/LoginActivity.kt
@@ -1,5 +1,10 @@
 package com.example.ticketit
 
-class LoginActivity : BaseActivity() {
+import android.os.Bundle
 
+class LoginActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
 }
